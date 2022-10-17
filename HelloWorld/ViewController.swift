c9104be1c5ec1123b2586f0bfb17b9acd09cab37
Maksim Makarov <m.s.makarov@gmail.com>
2022-10-17 20:50:02 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Максим Макаров on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
